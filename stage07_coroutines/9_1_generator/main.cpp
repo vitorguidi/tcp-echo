871a7fbe800a8f5e5dbb45b41a180ecf7d9489ce
Vitor Guidi <vitorguidi@gmail.com>
2026-06-16 22:46:08 -0700

    Add fibonacci coro example

--- stage07_coroutines/9_1_generator/main.cpp
+++ stage07_coroutines/9_1_generator/main.cpp
@@ -1,6 +1,52 @@
 #include <cstdio>
+#include <coroutine>
+#include <iostream>
+
+
+struct Task {
+    struct promise_type;
+    using handle_type = std::coroutine_handle<promise_type>;
+private:
+    handle_type h_;
+public:
+    struct promise_type {
+        int current_answer_ = 0;
+        Task get_return_object() {
+            return std::coroutine_handle<promise_type>::from_promise(*this);
+        }
+        std::suspend_always initial_suspend() { return {};}
+        std::suspend_always final_suspend() noexcept {return {};}
+        void unhandled_exception() {}
+        void return_void() {}
+        std::suspend_always yield_value(int x) {
+            current_answer_ = x;
+            return {};
+        }
+    };
+    Task(handle_type h) : h_(h) {}
+    ~Task() { if(h_) h_.destroy(); }
+
+    void resume() {h_.resume();}
+    bool done() const {return h_.done();}
+    int value() {return h_.promise().current_answer_;}
+
+};
+
+Task test(int n) {
+    if (n<0)    co_return;
+    int a=0, b =1;
+    for (int i=0; i<=n; ++i) {
+        co_yield a;
+        a+= b;
+        std::swap(a,b);
+    }
+}
 
 int main() {
-    // TODO: implement stage09_coroutines/9_1_generator
+    auto t = test(20);
+    for(int i=0;i<=20;i++) {
+        t.resume();
+        std::cout << t.value() << std::endl;
+    }
     return 0;
 }
